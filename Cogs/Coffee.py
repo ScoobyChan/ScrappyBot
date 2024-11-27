3d04ab26a6f0b78a193bbfe977ccfd189b1bb233
Scooby Chan <adam343fay@gmail.com>
2024-11-27 17:49:15 +1300

    fixed coffee

--- Cogs/Coffee.py
+++ Cogs/Coffee.py
@@ -1,5 +1,5 @@
 import json
-import urllib.request
+import requests
 import discord
 import random
 from discord.ext import commands
@@ -14,16 +14,16 @@ class Coffee(commands.Cog):
 	@commands.command()
 	async def coffee(self, ctx):
 		"""Coffee API will send coffe pics"""
-		with urllib.request.urlopen("https://coffee.alexflipnote.dev/random.json") as url:
-			data = json.loads(url.read().decode())
-			data = data['file']
+		response = requests.get("https://coffee.alexflipnote.dev/random.json")
+		if not response: raise Exception(f"Non-success status code: {response.status_code}")
+		data = response.json()
+		data = data['file']
 
-			if ctx.author.top_role.colour:
-				col = ctx.author.top_role.colour
-			else:
-				col =  random.choice(self.bot.color)
-			
-			embed=discord.Embed(title="coffee", color=col)
-			embed.set_image(url=data)
-			await ctx.send(embed=embed)
-
+		if ctx.author.top_role.colour:
+			col = ctx.author.top_role.colour
+		else:
+			col =  random.choice(self.bot.color)
+		
+		embed=discord.Embed(title="coffee", color=col)
+		embed.set_image(url=data)
+		await ctx.send(embed=embed)
